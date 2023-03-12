5a043c8249283c06e52b2e516b29b5fdbcff97fb
TomGana <tomgana7@gmail.com>
2023-03-12 14:57:28 -0400

    some stuff

--- Build_Your_Own_ETF/main.py
+++ Build_Your_Own_ETF/main.py
@@ -1,70 +1,77 @@
-import pandas as pd
-import numpy as np
-import requests
-#from get_all_tickers import get_tickers as gt
-import yfinance as yf
 
-def url(symbol: str, key):
-		return "https://financialmodelingprep.com/api/v3/market-capitalization/" + symbol + "?apikey=" + key
-
-def market_cap(key,tickers):
-
-	# Define lists of symbols and market caps
-	symbols = []
-	market_caps = []
-
-	# DataFrame to store all the data
-	data = pd.DataFrame(columns = ['Symbol', 'Market Cap'])
-
-	# Iterate based on the symbols list and add to DataFrame
-	for item in tickers:
-		response = requests.get(url(symbol = item, key = key)).json()
-
-		symbols.append(response[0]['symbol'])
-		market_caps.append(response[0]['marketCap'])
-    
-	# Add data to the dataframe
-	data['Symbol'] = symbols
-	data['Market Cap'] = market_caps
-	return data
-
-#def tickers():
-	#tickers = gt.get_tickers(AMEX=False)
-	#return tickers
-
-def bus():
-	#user input (defines which parameters to use)
-	#something that runs the necessary modules
+	#version1
+		#user input (defines which parameters to use)
+		#something that runs the necessary modules
 		#module() (gets list)
 		#module(n)( gets list)(n)
-	#returns final list
-	#display list to user
-	#
-	#
-	#logic for market cap
-	#+- 20% for 1 std up or below mean chosen by user
-	threshold = params["marketcap"]*.2
-	print(threshold)
+		#returns final list
+		#display list to user
+	#version2
+		# features
+		# update gets all needed data without needing a key so as not to get blacklisted or timed-out
+		#
+#                     import
+	import pandas as pd
+	import numpy as np
+	import requests
+	import yfinance as yf
+	from get_all_tickers import get_tickers as gt
 
+
+#                     utils
+	def update():
+		tickers_all = gt.get_tickers(AMEX=False)
+		tickers = yf.Tickers(tickers_all)
+		df = pd.DataFrame(tickers)
+		df.to_csv #to do: SAVE TO LOCATION
+		return df
+	def marketcap_module(df, params):
+		# Iterate through df and assign CAP weight based on distmap from target
+		for item in tickers:
+			df.item = 
+		return df
+	def dividend_module(df, params):
+		# Iterate through df and assign DIV weight based on distmap from target
+		for item in tickers:
+			df
+		return df
+	def price_to_earnings_module(df, params):
+		# Iterate through df and assign PE weight based on distmap from target
+		for item in tickers:
+			# gaussiandist out from target applying outward
+			# append new column
+			df=df_with_weight
+		return df
+#                     algos & bus
+	def controller(params, df):
+
+		for item in params
+			item.distmap = np.array[gaussiandist(len(df)]# define this based on gaussian dist
+			item.weight_max =  item(1)*item(2)# this is target but formatted
+			item.target  = item(1)# used in final solver
+		df = module(df,params)
+		df = price_to_earnings_module(df,params)
+		df = dividend_module(df,params)
+
+		# solver?????
+	def solver(params, df):
+		
+#                     run
+	def run():
 	
-	ticker_list = tickers()
-	data = market_cap(key,tickers)
-def run():
-	#get input
-	#gui()
-	msft = yf.Ticker("MSFT")
+		#                  input
+		test_params = {'CAP':[100000000,6],'DIV':[.2,4],'PE':[60,10]}
+		#                  update
+		if csv_date != today
+			df = update()
+		else
+		df = pd.read_csv("data.csv")
 
-	# get all stock info (slow)
-	data = msft.fast_info
-	test_params = {'marketcap':100000000,'div_yld':.2,'pe':60}
-	test_key = 'e761f6394296ca70082ffacff21f4733'
-	#return key as str return params as array
-	params_array =np.array([test_params['marketcap'], test_params['div_yld'],test_params['pe']])
-	#bus(params_array)
+		#                 controller
+		controller(params, df)
+		# slover??
 
-
-
-	print(data)
+		return df
 
 	
 
