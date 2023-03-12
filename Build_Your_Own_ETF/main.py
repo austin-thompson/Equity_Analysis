0c9e32c30aa16626d28188d1fc138a8cbb2e88c1
TomGana <tomgana7@gmail.com>
2023-03-11 22:31:24 -0500

    version 1

--- Build_Your_Own_ETF/main.py
+++ Build_Your_Own_ETF/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 import requests
-from get_all_tickers import get_tickers as gt
+#from get_all_tickers import get_tickers as gt
+import yfinance as yf
 
 def url(symbol: str, key):
 		return "https://financialmodelingprep.com/api/v3/market-capitalization/" + symbol + "?apikey=" + key
@@ -26,9 +28,9 @@ def market_cap(key,tickers):
 	data['Market Cap'] = market_caps
 	return data
 
-def tickers():
-	tickers = gt.get_tickers(AMEX=False)
-	return tickers
+#def tickers():
+	#tickers = gt.get_tickers(AMEX=False)
+	#return tickers
 
 def bus():
 	#user input (defines which parameters to use)
@@ -39,15 +41,26 @@ def bus():
 	#display list to user
 	#
 	#
-	params = {'marketcap':100000000}
-	key = 'e761f6394296ca70082ffacff21f4733'
-	#
+	#logic for market cap
+	#+- 20% for 1 std up or below mean chosen by user
 	threshold = params["marketcap"]*.2
 	print(threshold)
 
 	
 	ticker_list = tickers()
 	data = market_cap(key,tickers)
+def run():
+	#get input
+	#gui()
+	msft = yf.Ticker("MSFT")
+
+	# get all stock info (slow)
+	data = msft.fast_info
+	test_params = {'marketcap':100000000,'div_yld':.2,'pe':60}
+	test_key = 'e761f6394296ca70082ffacff21f4733'
+	#return key as str return params as array
+	params_array =np.array([test_params['marketcap'], test_params['div_yld'],test_params['pe']])
+	#bus(params_array)
 
 
 
@@ -55,4 +68,4 @@ def bus():
 
 	
 
-bus()
+run()
