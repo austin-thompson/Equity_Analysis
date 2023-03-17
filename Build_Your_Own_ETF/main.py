fad6d003e8ca68b1ef5ea8319a919d1e8c94c36a
server01home <ganatom16@gmail.com>
2023-03-17 14:27:21 -0400

    fixed some sudo code

--- Build_Your_Own_ETF/main.py
+++ Build_Your_Own_ETF/main.py
@@ -11,56 +11,59 @@
 		# update gets all needed data without needing a key so as not to get blacklisted or timed-out
 		#
 #                     import
-	import pandas as pd
-	import numpy as np
-	import requests
-	import yfinance as yf
-	from get_all_tickers import get_tickers as gt
+import pandas as pd
+import numpy as np
+import requests
+import yfinance as yf
+from get_all_tickers import get_tickers as gt
 
 
 #                     utils
-	def update():
-		tickers_all = gt.get_tickers(AMEX=False)
-		tickers = yf.Tickers(tickers_all)
-		for ticker in tickers:
-			df.append(ticker.fast_info)
-		df.to_csv #to do: SAVE TO LOCATION
-		return df
-	def param_module(dist, target_index):
+def update():
+	tickers_all = gt.get_tickers(AMEX=False)
+	tickers = yf.Tickers(tickers_all)
+	df = pd.DataFrame
+	for ticker in tickers:
+		df.append(ticker.fast_info)
+	df.to_csv #to do: SAVE TO LOCATION
+	return df
+def param_module(dist, target_index):
 		# Iterate through df and assign PE weight based on distmap from target
-		column_weight = []
-		dist_middle = len(dist)/2
-		for item in len(dist):
-			weight_score = dist[abs(dist_middle - abs(item_index - target_index))]
+	column_weight = []
+	dist_middle = len(dist)/2
+	for item in len(dist):
+		weight_score = dist[abs(dist_middle - abs(item.index.item() - target_index))]
 			# gaussiandist out from target applying outward
 			# append new column
-			column_weight.append(weight_score)
-		return column_weight
+		column_weight.append(weight_score)
+	return column_weight
 #                     algos & bus
-	def controller(params, df):
-		df.index.size
-		for param in params
-			targetvalue = params[param][0]
-			weight_max =  params[param][1]# this is target but formatted
-			df_length = len(df)*2
+def normal_dist(x,mean,sd):
+	prob_density = (np.pi*sd) * np.exp(-0.5*((x-mean)/sd)**2)
+	return prob_density
+
+def controller(params, df):
+	df.index.size
+	for param in params:
+		targetvalue = params[param][0]
+		weight_max =  params[param][1]# this is target but formatted
+		df_length = len(df)*2
 	#			distmap = [gauss*W]
-			x = np.linspace(1,df_length,df_length)
+		x = np.linspace(1,df_length,df_length)
 			#Creating a Function.
-			def normal_dist(x , mean , sd):
-				prob_density = (np.pi*sd) * np.exp(-0.5*((x-mean)/sd)**2)
-				return prob_density
+		
 			#Calculate mean and Standard deviation.
-			mean = np.mean(x)
-			sd = np.std(x)
+		mean = np.mean(x)
+		sd = np.std(x)
 			#Apply function to the data.
-			distmap = normal_dist(x,mean,sd)
-			distmap = (pdf/lendf)*weight_max
+		distmap = normal_dist(x,mean,sd)
+		distmap = (distmap/df_length)*weight_max
 	#			target_idx = index
-				targetdf = df[df.param == 2]
-				if target_df.index.size == 1
-					target_idx = targetdf.param.index.item()
-				else
-					target_idx = round(np.median(targetdf.param.index.values))		
+		target_df = df[df.param == 2]
+		if target_df.index.size == 1:
+			target_idx = target_df.param.index.item()
+		else:
+			target_idx = round(np.median(target_df.param.index.values))		
 	#			calls param module
 			param_column = param_module(distmap,target_idx)# centers dist map to target while keeping length of column len(df)
 			df.insert(df.columns.size-1, param+"W", param_column)
@@ -68,15 +71,15 @@
 
 	def solver(df, riskdist, budget):
 
-		for item in riskdist
+		for item in riskdist:
 			(item*budget)/df.(itemno).price=shares #calc shares
 			shares_column.append(shares) #add column with shares
 		df.append(shares_column)
 		df.column.sort(desc) 
-		for item in df
-			if item.shares >=1
+		for item in df:
+			if item.shares >=1:
 				etf_column = item.allinfo()
-			else
+			else:
 				break
 		return etf_column
 
@@ -87,10 +90,10 @@
 		#                  input
 		test_params = {'CAP':[100000000,4],'DIV':[.2,3],'PE':[60,3]}
 		#                  update
-		if csv_date != today
+		if csv_date != today:
 			df = update()
-		else
-		df = pd.read_csv("data.csv")
+		else:
+			df = pd.read_csv("data.csv")
 
 		#                 controller
 		#make params a dataframe
