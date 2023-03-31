2e694871354bf0f2c4dd55d7be983cdaaab7b082
Austin Thompson <austinthompsonnn@gmail.com>
2023-03-31 01:27:53 -0400

    remove 'Utilities/Grab_Ticker_Data/test.py'

--- Utilities/Grab_Ticker_Data/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import pandas_datareader as web
-df = pd.DataFrame({'TKR':['MSFT','AAPL','NVDA','CGNX','TM'],'CAP': [4, 6, 30,15,10],'DIV':[0,.1,.05,.2,.5],'PE':[10,60,80,30,20],'PRICE':[50,60,80,30,20]})
-
-print(df)
-
