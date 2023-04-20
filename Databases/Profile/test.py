9a191f82bfcfc08be336c3f0ef6e10bdb6be154a
TomGana <tomgana7@gmail.com>
2023-04-19 23:11:30 -0400

    profile test read and write

--- Databases/Profile/test.py
+++ Databases/Profile/test.py
@@ -1,16 +1,52 @@
-profile =    {
-                        "Id": "200",
-                        "Name": "Tom",
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+fileObject = open("mongourl.txt", "r")
+uri = fileObject.read()
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+
+db = client.finance_db  #Creating/accessing etf_db
+
+etf_collection = db.etf_collection
+
+
+
+profile_dict_test =    {
+                        "Name": "Tom's portfolio",
                         "Portfolio":   {
                                         "stock1": {
-                                                    "cap":[1000000000,4]
+                                                    "symbol":"MSFT",
+                                                    "cap":1000000000,
+                                                    "div":1.5,
+                                                    "pe":15,
+                                                    "shares":5
                                                         },
                                         "stock2": {
-                                                    "cap":[1,3]
+                                                    "symbol":"CGNX",
+                                                    "cap":20000,
+                                                    "div":.8,
+                                                    "pe":15,
+                                                    "shares":30
                                                         },
                                         "stock3": {
-                                                    "cap":[30,3]
+                                                    "symbol":"AAPL",
+                                                    "cap":1000000000,
+                                                    "div":1,
+                                                    "pe":15,
+                                                    "shares":5
                                                         }
                                         }
                     }
-print(profile)
+entry = etf_collection.insert_one(profile_dict_test)
+text_file = open("profile_id.txt", "w")
+n = text_file.write(str(entry.inserted_id))
+text_file.close()
+
+fileObject = open("profile_id.txt", "r")
+data = fileObject.read()
+print(data)
+find_id = etf_collection.find({"_id":data})
+print(find_id)
+
